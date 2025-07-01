efa06dce5f027e3aa78d03a7c0e3a68d4e74364c
malleboinasricharana <malleboinasricharana8@gmail.com>
2025-07-01 10:50:34 +0530

    Update maxelement.py
    changed into find 2nd largest number in the array

--- maxelement.py
+++ maxelement.py
@@ -1,6 +1,10 @@
-a=[1,9,5,7,3]
-max=a[0]
+a=[1,3,4,2,8]
+first=second=a[0]
 for i in range(len(a)):
-    if(a[i]>max):
-        max=a[i]
-print(max)
+    if(a[i]>first):
+        second=first
+        first=a[i]
+    elif(a[i]>second and a[i]>first):
+        second
+print(second)
+    
